23d8a985020e6b7f5db0aec59ab55d8cc16360eb
Eric <eric0521bigjohn@gmail.com>
2020-12-14 14:45:00 +0800

    update project tasks

--- src/components/project/Tasks.jsx
+++ src/components/project/Tasks.jsx
@@ -1,33 +1,69 @@
-// import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 // import { useSelector } from 'react-redux'
-// // import { auth, firestore } from '../../firebase'
+import { firestore } from "../../firebase";
 
-// const Tasks = (props) => {
-//   const state = useSelector((state) => state.Handleshowmember)
-//   // let [list, setList] = useState([])
-//   useEffect(() => {
-//     let reanageList = []
-//     let datalist = []
-//     // let joblist=[]
-//     state.member.forEach((item) => {
-//       let data = {
-//         user: item.displayName,
-//         userID: item.userID,
-//         tasks: [],
-//       }
-//       reanageList.push(data)
-//     })
-//     firestore
-//       .collection('subtasks')
-//       .where('project', '==', props.projectid)
-//       .onSnapshot((doc) => {
-//         doc.forEach((item) => {
-//           datalist.push(item.id)
-//         })
-//       })
-//   })
-//   console.log(props)
-//   console.log(state)
-//   return <div></div>
-// }
-// export default Tasks
+const Tasks = (props) => {
+  //   const state = useSelector((state) => state.Handleshowmember)
+  let [list, setList] = useState([]);
+  useEffect(() => {
+    // let reanageList = []
+    let datalist = [];
+    // state.member.forEach((item) => {
+    //   let data = {
+    //     user: item.displayName,
+    //     userID: item.userID,
+    //     tasks: [],
+    //   }
+    //   reanageList.push(data)
+    // })
+    firestore
+      .collection("subtasks")
+      .where("project", "==", props.projectid)
+      .onSnapshot(async (doc) => {
+        let list = [];
+        doc.forEach((item) => {
+          list.push(
+            firestore
+              .collection("subtasks")
+              .doc(item.id)
+              .collection("jobs")
+              .orderBy("createTime")
+              .get()
+              .then((data) => {
+                data.forEach((item) => {
+                  datalist.push(item.data());
+                });
+                return datalist;
+              })
+          );
+        });
+        console.log(list);
+        let a = await Promise.all(list);
+        let c = a[0];
+        c.sort((a, b) => (a.memberID > b.memberID ? 1 : -1));
+        setList(c);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  console.log(list);
+  return (
+    <div>
+      {list.map((item) => (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            width: "100%",
+          }}
+        >
+          <h1>{item.member}</h1>
+          <h1>{item.projectName}</h1>
+          <h1>{item.subTaskName}</h1>
+          <h1>{item.name}</h1>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default Tasks;
